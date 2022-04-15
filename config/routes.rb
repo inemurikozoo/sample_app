bb0149037f780e7ed614968decf26517f0f51e0f
inemurikozoo <100814669+inemurikozoo@users.noreply.github.com>
2022-04-15 05:02:25 +0000

    test commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   get 'lists/new'
+  # For details on the DSL available within this file,see https://guides.rubyonrails.org/routing.html
+  get 'top' => 'homes#top'
   post 'lists' => 'lists#create'
-  get 'lists/index'
-  get 'lists/show'
-  get 'lists/edit'
-  get '/top' => 'homes#top'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #理解メモ：URLを求められたとき＝＞〇コントローラの＃コマンドを実行。
+  #上記だと、URL'lists'を求められたとき、listsコントローラのcreateコマンドを実行する。
+  get 'lists' => 'lists#index'
+  # .../list/1や.../list/3に該当する
+  get 'lists/:id' => 'lists#show',as:'list'
 end
